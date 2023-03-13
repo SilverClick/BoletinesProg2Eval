06886498ff09c385e24205862ac1687790981616
SilverClick <113641305+SilverClick@users.noreply.github.com>
2023-03-13 18:34:23 +0100

    Add files via upload

---
+++ Boletin21/src/boletin21/Boletin21.java
@@ -0,0 +1,78 @@
+
+package boletin21;
+
+public class Boletin21 {
+package Boletin21;
+
+import com.gonzalo.datos.PedirDatos;
+import javax.swing.JOptionPane;
+
+
+public class Boletin21 {
+
+   
+    public static void main(String[] args) {
+        Metodos obx1 = new Metodos();
+        int auxNum[];
+        int auxNotas[] = null;
+        String auxNombres[] = null;
+        int numero;
+        do{
+            numero = PedirDatos.getInt("1. Array numeros\n2. Entrar en alumnos\n3. Calcular letra NIF\n4. Saír");
+            switch(numero){
+                case 1:
+                    auxNum = obx1.arrayNumeros();
+                    obx1.amosarArrayNumeros(auxNum);
+                    break;
+                case 2:
+                    int num = 0;
+                    do{
+                        num = PedirDatos.getInt("1. Crear alumnos con sus notas\n2. Numero de aprobados y suspensos\n3. Ver la media\n4. Ver nota más alta\n5. Buscar nota a partir del nombre\n6. Ver lista de alumnos aprobados\n7. Ordenar ascendete de notas\n8. Salir");
+                        switch(num){
+                            case 1:
+                                auxNotas = obx1.arrayNotas();
+                                auxNombres = obx1.arrayNombre();
+                                break;
+                            case 2:
+                                obx1.amosarNotas(auxNotas);
+                                break;
+                            case 3:
+                                obx1.amosarNotaMedia(auxNotas);
+                                break;
+                            case 4:
+                                obx1.amosarNotaAlta(auxNotas);
+                                break;
+                            case 5:
+                                obx1.amosarNotaDeterminada(auxNombres, auxNotas);
+                                break;
+                            case 6:
+                                obx1.amosarAprobados(auxNombres, auxNotas);
+                                break;
+                            case 7:
+                                obx1.ordernarLista(auxNombres, auxNotas);
+                                break;
+                            default:
+                                JOptionPane.showMessageDialog(null, "Saíndo das notas");
+                        }
+                        
+                    }while (num > 0 && num < 8);
+                    break;
+                case 3:
+                    obx1.calcularLetraNIF();
+                    break;
+                case 4:
+                    System.out.println("Saída do programa");
+                    break;
+                default:
+                    System.out.println("Orden no encontrada, saíndo do programa");
+                    break;                    
+            }
+        }while(numero < 4 && numero > 0);
+        
+    }
+    
+}
+    public static void main(String[] args) {
+           }
+    
+}
---
+++ Boletin21/src/boletin21/Metodos.java
@@ -0,0 +1,151 @@
+
+package boletin21;
+
+
+public class Metodos {
+    import com.gonzalo.datos.PedirDatos;
+
+
+    private int CTE = 30;
+    
+    //Ejercicio 1
+    public int[] arrayNumeros(){
+        int numeros[] = new int[6];
+        for (int i = 0; i < numeros.length; i++){
+            int num = (int) (Math.random()*50+1);
+            for (int j = i; j > 0; j--){
+                if(numeros[j] == num){
+                    num = (int) (Math.random()*50+1);
+                    j = i;
+                }
+            }
+            numeros[i] = num;            
+        }
+        return numeros;        
+    }
+    
+    //Ejercicio 2,3
+    public void amosarArrayNumeros(int numeros[]){
+        for(int i = (numeros.length-1); i>=0; i--){
+            System.out.println("O numero da " + (i + 1) + "º posicion = " + numeros[i]);
+        }
+    }
+    
+    public int[] arrayNotas(){
+        int notas[] = new int[CTE];
+        for (int i = 0; i < notas.length; i++){
+            int num = (int) (Math.random()*10+1);
+            notas[i] = num;
+        }
+        return notas;
+    }    
+    
+    public String[] arrayNombre(){
+        String nombres[] = new String[CTE];
+        for (int i = 0; i < nombres.length; i++){
+            nombres[i] = PedirDatos.getString("Teclee o nome do " + (i+1) +"º alumno");
+        }
+        return nombres;
+    }
+    
+    public void amosarNotas(int notas[]){
+        int aprobados = 0;
+        int suspensos = 0;
+        for (int i = 0; i < notas.length; i++){
+            if(notas[i] >= 5){
+                aprobados++;
+            }
+            else{
+                suspensos++;
+            }
+        }
+        System.out.println("Numeros de aprobados = " + aprobados + "\nNumeros de suspensos = " + suspensos);
+    }
+    
+    public void amosarNotaMedia(int notas[]){
+        int suma = 0;
+        for (int i = 0; i < notas.length; i++){
+            suma = suma + notas[i];
+        }
+        float media = (float) suma/(notas.length);
+        System.out.println("A nota media é de: " + media);
+    }
+    
+    public void amosarNotaAlta(int notas[]){
+        int notaAlta = 0;
+        for (int i = 0; i < notas.length; i++){
+            if(notaAlta < notas[i]){
+                notaAlta = notas[i];
+            }
+           
+        }
+        System.out.println("A nota máis alta é " + notaAlta);
+    }
+    
+    public void amosarNotaDeterminada(String nombres[], int notas[]){
+        String nombreAlum = PedirDatos.getString("Teclee o alumno a ver");
+        int num = 0;
+        for(int i = 0; i < nombres.length; i++){
+            if(nombres[i].equalsIgnoreCase(nombreAlum)){
+                System.out.println(nombres[i] + " Ten un " + notas[i] + " de nota");
+                num = 1;
+            }
+        }
+        if(num == 0){
+            System.out.println("Nome no encontrado");
+        }
+    }
+    
+    public void amosarAprobados(String nombres[], int notas[]){
+        System.out.println("Aprobaron:");
+        int num = 0;
+        for (int i = 0; i < notas.length; i++){
+            if(notas[i] >= 5){
+                System.out.println((i+1) + "º alumno: " + nombres[i] + " con un " + notas[i]);
+                num = 1;
+            }
+        }
+        if (num == 0){
+            System.out.println("No aprobó ninguén");
+        }
+    }
+    
+    public void ordernarLista(String nombres[], int notas[]){
+        String auxN;
+        int aux;
+        for(int i = 0; i < CTE-1; i++){
+            for(int j = i+1; j < CTE ; j++){
+                if(notas[i] > notas[j]){
+                    aux = notas[i];
+                    auxN = nombres[i];
+                    notas[i] = notas[j];
+                    nombres[i] = nombres[j];
+                    notas[j] = aux;
+                    nombres[j] = auxN;
+                }
+            }
+        }
+        System.out.println("Lista ordeada:");
+        for(int i = 0; i < notas.length; i++){
+            System.out.println(nombres[i] + ": " + notas[i]);
+        }
+    }
+    
+    //Ejercicio 4
+    public void calcularLetraNIF(){
+        long num;
+        num = PedirDatos.getLong("Teclee o numero do teu NIF");
+        while(num < 10000000 || num > 100000000){
+            num = PedirDatos.getLong("O numero do NIF ten que ter 8 digitos, sendo 10000000 a primera combinacion posible ");
+        }
+        float div = num%23;
+        int resto = (int) div;
+        char letra[] = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        for (int i = 0; i<letra.length; i++){
+            if (resto == i){
+                System.out.println("Tu letra es: " + letra[i]);
+            }
+        }
+    }
+}
+
